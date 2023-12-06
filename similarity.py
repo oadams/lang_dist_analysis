7ff1f72f1baf4c144a0402d71cf332d037ee7e7c
Oliver Adams <oliver.adams@gmail.com>
2023-12-07 10:48:31 +1100

    Started on code to extract language embeddings from commonvoice data.

---
+++ similarity.py
@@ -0,0 +1,59 @@
+from pathlib import Path
+
+import torch
+import tqdm
+from speechbrain.pretrained import EncoderClassifier
+
+classifier = EncoderClassifier.from_hparams(source="speechbrain/lang-id-commonlanguage_ecapa", savedir="pretrained_models/lang-id-commonlanguage_ecapa")
+# Italian Example
+#out_prob, score, index, text_lab = classifier.classify_file('speechbrain/lang-id-commonlanguage_ecapa/example-it.wav')
+#print(text_lab)
+
+# French Example
+#out_prob, score, index, text_lab = classifier.classify_file('speechbrain/lang-id-commonlanguage_ecapa/example-fr.wav')
+#print(text_lab)
+
+def embed_audio(audio_path, classifier):
+    waveform = classifier.load_audio(audio_path)
+    # Fake a batch:gg
+    batch = waveform.unsqueeze(0)
+    rel_length = torch.tensor([1.0])
+    return classifier.encode_batch(batch, rel_length)
+
+#print(embed_audio('speechbrain/lang-id-commonlanguage_ecapa/example-it.wav', classifier).shape)
+
+language_codes = [
+    "ar", "eu", "br", "ca", "zh", "zh", "zh", "cv", "cs", "dv", 
+    "nl", "en", "eo", "et", "fr", "fy", "ka", "de", "el", "cnr", 
+    "id", "ia", "it", "ja", "kab", "rw", "ky", "lv", "mt", "mn", 
+    "fa", "pl", "pt", "ro", "rm", "ru", "sah", "sl", "es", "sv", 
+    "ta", "tt", "tr", "uk", "cy"
+]
+
+#for code in language_codes:
+#    embed_audio(f'speechbrain/lang-id-commonlanguage_ecapa/example-{code}.wav', classifier)
+#    out_prob, score, index, text_lab = classifier.classify_file('speechbrain/lang-id-commonlanguage_ecapa/example-fr.wav')
+#    print(text_lab)
+
+from torch.nn.utils.rnn import pad_sequence
+
+
+def encode_lang(lang, split, classifier):
+    batch = []
+    lengths = []
+    lang_path = Path('common_voice_kpd') / lang
+    for audio_path in (lang_path / split).glob('**/*.wav'):
+        waveform = classifier.load_audio(str(audio_path))
+        batch.append(waveform)
+        lengths.append(waveform.shape[0])
+    lengths = torch.tensor(lengths)
+    rel_lengths = lengths / lengths.max()
+    batch = pad_sequence(batch, batch_first=True, padding_value=0)
+    emb = classifier.encode_batch(batch, rel_lengths)
+    emb_dir = Path('lang_embs')
+    emb_dir.mkdir(exist_ok=True)
+    torch.save(emb, emb_dir / f'{lang}_{split}_emb.pt')
+
+for path in tqdm.tqdm(list(Path('common_voice_kpd').glob('*'))):
+    lang = path.name
+    encode_lang(lang, 'test', classifier)
